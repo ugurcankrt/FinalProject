8f243d4cca57b00a5118d66ea199df3491740f4b
Ayşegül Işık <18010207065@ogrenci.bartin.edu.tr>
2022-08-15 18:35:54 +0300

    Proje dosyası ekle.

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{       
+        //class:referans tip olmalı
+        //ientity: ientity olabilir veya ientityi implement alan classlar
+        //new(): newlenebilir olmalı yani ientity kullanılamaz interface olduğu için
+        //core diğer katmanları referans almaz!!!!
+
+    public interface IEntityRepository<T> where T:class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,79 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.IntMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product { ProductId = 1, CategoryId = 1, ProductName = "Bardak", UnitPrice = 15, UnitsInStock = 15 },
+                new Product { ProductId = 2, CategoryId = 1, ProductName = "Masa", UnitPrice = 150, UnitsInStock = 4 },
+                new Product { ProductId = 3, CategoryId = 2, ProductName = "Kalem", UnitPrice = 12, UnitsInStock = 111 },
+                new Product { ProductId = 4, CategoryId = 2, ProductName = "Defter", UnitPrice = 16, UnitsInStock = 222 },
+                new Product { ProductId = 5, CategoryId = 2, ProductName = "Pantolon", UnitPrice = 115, UnitsInStock = 17 }
+            };
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId== product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p=>p.CategoryId== categoryId).ToList();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+       
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+            productToUpdate.UnitPrice = product.UnitPrice;
+        }
+
+    }
+}
+
+
+      
